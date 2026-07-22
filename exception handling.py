b30c0b39e993b101a6c7b3474d925287c02bedb0
jahnavi18-20 <jahnaviatmakuri@gmail.com>
2026-07-22 16:18:07 +0530

    Add files via upload

---
+++ exception handling.py
@@ -0,0 +1,11 @@
+#except
+class division():
+    try:
+        x=10/5
+        print('divisible',x)
+    except ZeroDivisionEror:
+        print('error occured')
+
+#list of these files
+import os
+print(os.mkdir)
